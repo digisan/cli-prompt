081488df8998f2cc58fa86ca1be80d176d36ff17
Qing Miao <digisanQM@outlook.com>
2021-09-04 08:58:35 +1000

    * TODO: input error handle

--- cli-prompt.go
+++ cli-prompt.go
@@ -3,53 +3,74 @@ package cliprompt
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 
+	"github.com/digisan/gotk/slice/ts"
 	jt "github.com/digisan/json-tool"
 )
 
-func AnalyzeConfig(cfgpath string) error {
+func AnalyzeConfig(configPath string) (map[string]interface{}, error) {
 
-	bytes, err := os.ReadFile(cfgpath)
+	bytes, err := os.ReadFile(configPath)
 	if err != nil {
-		return err
+		return nil, err
 	}
+
+	r := regexp.MustCompile(`"_\w+":`)
+	prompts := r.FindAllString(string(bytes), -1)
+	prompts = ts.FM(prompts, nil, func(i int, e string) string { return e[2 : len(e)-2] })
 
 	m, err := jt.Flatten(bytes)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	fmt.Println("Input needed arguments, if same as default value, just <ENTRE>")
+	//
+	// check config value & type
+	//
+	// for k, v := range m {
+	// 	fmt.Printf("%v(%T) - %v(%T)\n", k, k, v, v)
+	// }
 
-	fields := []string{}
-	prompts := []string{}
-
-	for k := range m {
-		if !strings.HasPrefix(k, "_") {
-			fields = append(fields, k)
-		} else {
-			prompts = append(prompts, k)
-		}
-	}
+	fmt.Println(`
+--------------------------------------------------------------
+input needed arguments, if default value applies, just <ENTRE>
+--------------------------------------------------------------`)
 
 	for _, f := range prompts {
-		fv := f[1:]
-		fmt.Printf("%v, default value is [%v]: ", m[f], m[fv])
-		var value string
-		n, err := fmt.Scanf("%v", &value)
+
+		var fVal interface{} = m[f]
+		fmt.Printf("\n--> %v, default is [%v]: ", m["_"+f], fVal)
+
+		var iVal string
+		n, err := fmt.Scanf("%v", &iVal)
 		if n == 0 {
 			continue
 		}
 		if err != nil {
 			panic(err)
 		}
-		m[fv] = value
+
+		switch fVal.(type) {
+		case int, float64:
+			m[f], err = strconv.ParseInt(iVal, 10, 64)
+			if err != nil {
+				m[f], err = strconv.ParseFloat(iVal, 64)
+			}
+		default:
+			m[f] = iVal
+		}
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	fmt.Println("----------------")
 
 	cfg := jt.Composite(m, func(path string) bool { return !strings.HasPrefix(path, "_") })
 	fmt.Println(jt.FmtStr(cfg, "   "))
-	return nil
+
+	return m, nil
 }
